c2c0c9d968a4629b78147370e2f38c5d632921e6
Martin William <martinsoftwaredev@outlook.com>
2023-02-15 12:10:13 +0000

    change old prices codes to words in prices.js

--- src/commands/mongo/prices.js
+++ src/commands/mongo/prices.js
@@ -25,75 +25,75 @@ module.exports = {
             .addFields([
                 {
                     name: "<:Gold:1020795638864171140> Gold",
-                    value: latestPrices.rates.XAU.toPrecision(5),
+                    value: latestPrices.rates.gold.toPrecision(5),
                     inline: true
                 },
                 {
                     name: "<:Silver:1020795717763215441> Silver",
-                    value: latestPrices.rates.XAG.toPrecision(5),
+                    value: latestPrices.rates.silver.toPrecision(5),
                     inline: true
                 },
                 {
                     name: "<:Platinum:1020795714227408936> Platinum",
-                    value: latestPrices.rates.XPT.toPrecision(5),
+                    value: latestPrices.rates.platinum.toPrecision(5),
                     inline: true
                 },
                 {
                     name: "<:Palladium:1020795713036230777> Palladium",
-                    value: latestPrices.rates.XPD.toPrecision(5),
+                    value: latestPrices.rates.palladium.toPrecision(5),
                     inline: true
                 },
                 {
                     name: "<:Rhodium:1020795715389243503> Rhodium",
-                    value: latestPrices.rates.XRH.toPrecision(5),
+                    value: latestPrices.rates.rhodium.toPrecision(5),
                     inline: true
                 },
                 {
                     name: "<:Ruthenium:1020795716249059339> Ruthenium",
-                    value: latestPrices.rates.RUTH.toPrecision(5),
+                    value: latestPrices.rates.ruthenium.toPrecision(5),
                     inline: true
                 },
                 {
                     name: "<:Copper:1020795706765738084> Copper",
-                    value: latestPrices.rates.XCU.toPrecision(5),
+                    value: latestPrices.rates.copper.toPrecision(5),
                     inline: true
                 },
                 {
                     name: "<:Nickel:1020795711585009666> Nickel",
-                    value: latestPrices.rates.NI.toPrecision(5),
+                    value: latestPrices.rates.nickel.toPrecision(5),
                     inline: true
                 },
                 {
                     name: "<:Aluminium:1020795703989116949> Aluminium",
-                    value: latestPrices.rates.ALU.toPrecision(5),
+                    value: latestPrices.rates.aluminium.toPrecision(5),
                     inline: true
                 },
                 {
                     name: "<:Zinc:1020795720527249478> Zinc",
-                    value:latestPrices.rates.ZNC.toPrecision(5),
+                    value:latestPrices.rates.zinc.toPrecision(5),
                     inline: true
                 },
                 {
                     name: "<:Tin:1020795718807601224> Tin",
-                    value: latestPrices.rates.TIN.toPrecision(5),
+                    value: latestPrices.rates.tin.toPrecision(5),
                     inline: true
                 },
                 {
                     name: "<:Cobalt:1020795705587154966> Cobalt",
-                    value: latestPrices.rates.LCO.toPrecision(5),
+                    value: latestPrices.rates.cobalt.toPrecision(5),
                     inline: true
                 },{
                     name: "<:Iridium:1020795708191809576> Iridium",
-                    value: latestPrices.rates.IRD.toPrecision(5),
+                    value: latestPrices.rates.iridium.toPrecision(5),
                     inline: true
                 },{
                     name: "<:Lead:1020795710767112323> Lead",
-                    value: latestPrices.rates.LEAD.toPrecision(5),
+                    value: latestPrices.rates.lead.toPrecision(5),
                     inline: true
                 },
                 {
                     name: "<:Iron:1020795709068423199> Iron",
-                    value: latestPrices.rates.IRON.toPrecision(5),
+                    value: latestPrices.rates.iron.toPrecision(5),
                     inline: true
                 },
             ]);
